60c44033df2e19a10a952276b16f9c5761e6cda8
bore521@163.com <xie9210>
2018-04-12 15:33:45 +0800

    Initial commit

---
+++ glide/src/main/java/me/ibore/glide/XGlide.java
@@ -0,0 +1,14 @@
+package me.ibore.glide;
+
+public class XGlide {
+
+    public static void init() {
+
+    }
+
+    public static void getCacheImage(String url) {
+//        DataCacheKey dataCacheKey = new DataCacheKey(new GlideUrl(url), EmptySignature.obtain());
+//        String safeKey = SafeKeyGenerator.getSafeKey(dataCacheKey);
+    }
+
+}
